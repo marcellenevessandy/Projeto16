a3b6a0c11125916d108f9bc518cc8b15d357f590
Dell <Dell@DESKTOP-20C7A40>
2023-10-18 23:17:04 -0200

    Projeto 16, while1.

---
+++ src/while1/While1.java
@@ -0,0 +1,20 @@
+package while1;
+
+public class While1 {
+
+    public static void main(String[] args) {
+        int i;
+        i=1;
+
+        while (i <=6) {
+        if(i%2==0){
+       
+        System.out.println(i);
+        i++;
+        }
+        }
+    }
+}
+
+
+
